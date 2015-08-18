c0c231a16a8118f49e9c5e79000666d557a5272e
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-19 05:21:54 +0800

    make index and functions

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once('config.php');
+require_once('functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>お問い合わせフォーム</title>
+	</head>
+	<body>
+		<h1>お問い合わせフォーム</h1>
+	</body>
+</html>
