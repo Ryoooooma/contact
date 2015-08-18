14d6fd9a34685ee7869544360d690e110d52b948
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-19 05:22:28 +0800

    make form

--- index.php
+++ index.php
@@ -13,5 +13,12 @@ require_once('functions.php');
 	</head>
 	<body>
 		<h1>お問い合わせフォーム</h1>
+		<form action="" method="post">
+			<p>お名前：<input type="text" name="name" value=""></p>
+			<p>メールアドレス*：<input type="text" name="email" value=""></p>
+			<p>内容*；</p>
+			<p><textareatype="textarea" name="memo" cols="40" rows="5"></textarea></p>
+			<p><input type="submit" value="送信"></p>
+		</form>
 	</body>
 </html>
