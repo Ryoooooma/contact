9c6bcf306fac55ce5c990ba73ba4805a42fc83e5
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-21 15:46:18 +0800

    insert into entries and create thank you page

---
+++ admin/index.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('../config.php');
+require_once('../functions.php');
+
+$dbh = connectDb();
+
+$entries = array();
+
+$sql = "select * from entries order by created desc";
+
+foreach ($dbh->query($sql) as $row) {
+	array_push($entries, $row);
+}
+
+// var_dump($entries);
+
+// exit;
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>お問い合わせ一覧</title>
+		<link rel="stylesheet" type="text/css" href="../style.css">
+	</head>
+	<body>
+		<h1>データ一覧</h1>
+		<p>お問い合わせ件数：<?php echo count($entries); ?>件</p>
+		<ul>
+			<?php foreach ($entries as $entry) : ?>
+				<li>
+					<?php echo h($entry['email']); ?>
+					<a href="edit.php?id=<?phpecho h($entry['id']); ?>">[削除]</a>
+					<span class="deleteLink" data-id="<?php echo h($entry['id']); ?>">[削除]</span>
+				</li>
+			<?php endforeach ; ?>
+		</ul>
+		<p><a href="<?php echo SITE_URL; ?>">お問い合わせフォームへ</a></p>
+	</body>
+</html>
--- database.sql
+++ database.sql
@@ -1,30 +1,29 @@
+<?php
 
- -- database memo
- create database votation_dotinstall;
+// database memo
+// create database contact_dotinstall;
 
- grant all on votation_dotinstall.* to dbuser@localhost identified by 'rwrwrwrw0521';
+// grant all on contact_dotinstall.* to dbuser@localhost identified by 'rwrwrwrw0521';
 
 
- use votation_dotinstall;
+// use contact_dotinstall;
 
- create table answers (
- 	id int not null auto_increment primary key,
- 	answer varchar(255),
- 	remote_address varchar(15),
- 	user_agent varchar(255),
- 	answer_date date,
- 	created datetime,
- 	modified datetime
- 	unique unique_answer (remote_address, user_agent, answer_date)
- );
+// create table entries (
+// 	id int not null auto_increment primary key,
+// 	name varchar(255),
+// 	email varchar(255),
+// 	memo text,
+// 	created datetime,
+// 	modified datetime
+// );
 
--- define('DSN', 'mysql:host=localhost;dbname=contact_dotinstall');
--- define('DB_USER', 'dbuser');
--- define('DB_PASSWORD', 'rwrwrwrw0521');
+define('DSN', 'mysql:host=localhost;dbname=contact_dotinstall');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'rwrwrwrw0521');
 
--- define('SITE_URL', '192.168.33.10/contact/index.php');
--- define('ADMIN_URL', SITE_URL.'admin/');
+define('SITE_URL', '192.168.33.10/contact/index.php');
+define('ADMIN_URL', SITE_URL.'admin/');
 
--- error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);
 
--- session_set_cookie_params(0, '/contact/');
+session_set_cookie_params(0, '/contact/');
