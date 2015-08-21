3cdcae0daee4f22fa90814290720aa9c0c2d223e
Ryoma Wakita <your.ryoma.wakita@gmail.com>
2015-08-21 16:56:21 +0800

    complete delete function

--- admin/index.php
+++ admin/index.php
@@ -24,20 +24,53 @@ foreach ($dbh->query($sql) as $row) {
 	<head>
 		<meta charset="utf-8">
 		<title>お問い合わせ一覧</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="../style.css">
 	</head>
 	<body>
 		<h1>データ一覧</h1>
-		<p>お問い合わせ件数：<?php echo count($entries); ?>件</p>
+		<p><span id="num">お問い合わせ件数：<?php echo count($entries); ?></span>件</p>
 		<ul>
 			<?php foreach ($entries as $entry) : ?>
-				<li>
+				<li id="entry_<?php echo h($entry['id']); ?>">
 					<?php echo h($entry['email']); ?>
-					<a href="edit.php?id=<?phpecho h($entry['id']); ?>">[削除]</a>
+					「<?php echo h($entry['memo']); ?>」
+					<a href="edit.php?id=<?phpecho h($entry['id']); ?>">[編集]</a>
 					<span class="deleteLink" data-id="<?php echo h($entry['id']); ?>">[削除]</span>
 				</li>
 			<?php endforeach ; ?>
 		</ul>
 		<p><a href="<?php echo SITE_URL; ?>">お問い合わせフォームへ</a></p>
+		<script>
+			$(function() {
+				$('.deleteLink').click(function() {
+					if (confirm("削除してもよろしいですか？")) {
+						var num = $('#num').text();
+						num--;
+						$.post('./delete.php', {
+							id: $(this).data('id')
+						}, function(rs) {
+							$('#entry_' + rs).fadeOut(800);
+							// ここ使うと削除した時にバグが起きる→要検証！
+							// $('#num').text(num);
+						});
+					}
+				});
+			});
+		</script>
 	</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
